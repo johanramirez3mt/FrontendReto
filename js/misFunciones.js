fd844cff0ae97e72ca1b2fb94ad6194cc76de18e
gocyberman <alejodc2003@gmail.com>
2021-10-21 20:48:12 -0500

    Se ocultan botones de opciones de las grillas

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -1,11 +1,83 @@
+function cargarInfoCategoria(){
+    $.ajax({
+        type:"GET",
+        contentType: "application/json; charset=utf-8",
+        datatype:"JSON",
+        url:"http://localhost:8080/api/Category/all",
+        success:function(respuesta){
+            console.log(respuesta);
+            cargarRespuestaCategoria(respuesta);
+        }
+    });
+}
+
+function cargarRespuestaCategoria(items){
+    $("tbody").children().remove()
+    var tableBody = $('#tblCategorias tbody');
+    var filaTabla = "";
+    for (var i=0; i<items.length; i++) {
+        filaTabla += "<tr>";
+        filaTabla += "<td>"+items[i].id+"</td>";
+        filaTabla += "<td>"+items[i].name+"</td>";
+        filaTabla += "<td>"+items[i].description+"</td>";
+        //filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick=''>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick=''>Eliminar</button> </td>";
+        filaTabla +="</tr>";
+    }
+    tableBody.append(filaTabla);
+}
+
+function newCategoria(){
+    let data={
+        // id:$("#id").val(),
+        name:$("#name").val(),
+        description:$("#description").val(),
+    };
+    let dataToSend=JSON.stringify(data);
+    $.ajax({
+        type:"POST",
+        contentType: "application/json; charset=utf-8",
+        data:dataToSend,
+        datatype:"JSON",
+        url:"http://localhost:8080/api/Category/save",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#name").val("");
+            $("#description").val("");
+            cargarInfoCategoria();
+            alert("Se Guardo Categoria Exitosamente")
+        }
+    });
+}
+
+/* ------------------------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------------------------------------------- */
+
 function cargarInfoBarcos(){
     $.ajax({
-        url:"https://gb6afec73d437f3-dbreto1alejod.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/boat/boat",
+        type:"GET",
+        contentType: "application/json; charset=utf-8",
+        datatype:"JSON",
+        url:"http://localhost:8080/api/Boat/all",
+        success:function(respuesta){
+            console.log(respuesta);
+            cargarRespuestaBarcos(respuesta);
+        }
+    });
+}
+
+function selectCategoria(){
+    $.ajax({
+        url:"http://localhost:8080/api/Category/all",
         type:"GET",
         datatype:"JSON",
         success:function(respuesta){
-            console.log(respuesta);
-            cargarRespuestaBarcos(respuesta.items);
+            respuesta.forEach(function(item){
+                var option = document.createElement('option');
+                option.value = item.id;
+                option.innerHTML = item.name;
+                category_id.appendChild(option)
+                })
         }
     });
 }
@@ -18,39 +90,49 @@ function cargarRespuestaBarcos(items){
         filaTabla += "<tr>";
         filaTabla += "<td>"+items[i].id+"</td>";
         filaTabla += "<td>"+items[i].brand+"</td>";
-        filaTabla += "<td>"+items[i].model+"</td>";
-        filaTabla +="<td>"+items[i].category_id+"</td>";
+        filaTabla += "<td>"+items[i].year+"</td>";
+        filaTabla +="<td>"+items[i].category.name+"</td>";
         filaTabla +="<td>"+items[i].name+"</td>";
-        filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick='passEditBarco()'>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick='deleteBarco("+items[i].id+")'>Eliminar</button> </td>";
+        filaTabla +="<td>"+items[i].description+"</td>";
+        //filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick=''>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick=''>Eliminar</button> </td>";
         filaTabla +="</tr>";
     }
     tableBody.append(filaTabla);
 }
 
 function newBarco(){
-    let data={
-        id:$("#id").val(),
-        brand:$("#brand").val(),
-        model:$("#model").val(),
-        category_id:$("#category_id").val(),
-        name:$("#name").val()
-    };
-    $.ajax({
-        url:"https://gb6afec73d437f3-dbreto1alejod.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/boat/boat",
-        type:"POST",
-        data:data,
-        datatype:"JSON",
-        success:function(respuesta){
-            $("#resultado").empty();
-            $("#id").val("");
-            $("#brand").val("");
-            $("#model").val("");
-            $("#category_id").val("");
-            $("#name").val("");
-            cargarInfoBarcos();
-            alert("Se Guardo Barco Exitosamente")
-        }
-    });
+    if($("#category_id").val()){
+        let objCategory = {id:$("#category_id").val()};
+        let data={
+            // id:$("#id").val(),
+            brand:$("#brand").val(),
+            year:$("#model").val(),
+            category:objCategory,
+            name:$("#name").val(),
+            description:$("#description").val()
+        };
+        let dataToSend=JSON.stringify(data);
+        $.ajax({
+            type:"POST",
+            contentType: "application/json; charset=utf-8",
+            data:dataToSend,
+            datatype:"JSON",
+            url:"http://localhost:8080/api/Boat/save",
+            success:function(respuesta){
+                $("#resultado").empty();
+                $("#id").val("");
+                $("#brand").val("");
+                $("#model").val("");
+                $("#category_id").val("");
+                $("#name").val("");
+                $("#description").val("");
+                cargarInfoBarcos();
+                alert("Se Guardo Barco Exitosamente")
+            }
+        });
+    }else {
+        alert("ojo, Debe seleccionar una categoria")
+}
 }
 
 function passEditBarco(){
@@ -124,12 +206,12 @@ function deleteBarco(idElemento){
 
 function cargarInfoClient(){
     $.ajax({
-        url:"https://gb6afec73d437f3-dbreto1alejod.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/client/client",
         type:"GET",
         datatype:"JSON",
+        url:"http://localhost:8080/api/Client/all",
         success:function(respuesta){
             console.log(respuesta);
-            cargarRespuestaClient(respuesta.items);
+            cargarRespuestaClient(respuesta);
         }
 
     });
@@ -141,11 +223,12 @@ function cargarRespuestaClient(items){
     var filaTabla = "";
     for (var i=0; i<items.length; i++) {
         filaTabla += "<tr>";
-        filaTabla += "<td>"+items[i].id+"</td>";
+        filaTabla += "<td>"+items[i].idClient+"</td>";
         filaTabla += "<td>"+items[i].name+"</td>";
         filaTabla += "<td>"+items[i].email+"</td>";
         filaTabla +="<td>"+items[i].age+"</td>";
-        filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick='passEditCliente()'>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick='deleteClient("+items[i].id+")'>Eliminar</button> </td>";
+        filaTabla +="<td>"+items[i].password+"</td>";
+        //filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick=''>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick=''>Eliminar</button> </td>";
         filaTabla +="</tr>";
     }
     tableBody.append(filaTabla);
@@ -153,22 +236,26 @@ function cargarRespuestaClient(items){
 
 function newClient(){
     let data={
-        id:$("#id").val(),
+        idClient:$("#id").val(),
         name:$("#name").val(),
         email:$("#email").val(),
-        age:$("#age").val()
+        age:$("#age").val(),
+        password:$("#password").val()
     };
+    let dataToSend=JSON.stringify(data);
     $.ajax({
-        url:"https://gb6afec73d437f3-dbreto1alejod.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/client/client",
         type:"POST",
-        data:data,
+        contentType: "application/json; charset=utf-8",
+        data:dataToSend,
         datatype:"JSON",
+        url:"http://localhost:8080/api/Client/save",
         success:function(respuesta){
             $("#resultado").empty();
             $("#id").val("");
             $("#name").val("");
             $("#email").val("");
             $("#age").val("");
+            $("#password").val("");
             cargarInfoClient();
             alert("Se Agrego Nuevo Cliente Exitosamente")
         }
@@ -240,12 +327,13 @@ function deleteClient(idElemento){
 
 function cargarInfoMessage(){
     $.ajax({
-        url:"https://gb6afec73d437f3-dbreto1alejod.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/message/message",
         type:"GET",
+        contentType: "application/json; charset=utf-8",
         datatype:"JSON",
+        url:"http://localhost:8080/api/Message/all",
         success:function(respuesta){
             console.log(respuesta);
-            cargarRespuestaMessage(respuesta.items);
+            cargarRespuestaMessage(respuesta);
         }
 
     });
@@ -257,32 +345,81 @@ function cargarRespuestaMessage(items){
     var filaTabla = "";
     for (var i=0; i<items.length; i++) {
         filaTabla += "<tr>";
-        filaTabla += "<td>"+items[i].id+"</td>";
-        filaTabla += "<td>"+items[i].messagetext+"</td>";
-        filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick='passEditMensaje()'>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick='deleteMessage("+items[i].id+")'>Eliminar</button> </td>";
+        filaTabla += "<td>"+items[i].idMessage+"</td>";
+        filaTabla += "<td>"+items[i].messageText+"</td>";
+        filaTabla += "<td>"+items[i].client.name+"</td>";
+        filaTabla += "<td>"+items[i].boat.name+"</td>";
+        //filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick='passEditMensaje()'>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick='deleteMessage("+items[i].id+")'>Eliminar</button> </td>";
         filaTabla +="</tr>";
     }
     tableBody.append(filaTabla);
 }
 
-function newMessage(){
-    let data={
-        id:$("#id").val(),
-        messagetext:$("#messagetext").val()
-    };
+function selectClientes(){
     $.ajax({
-        url:"https://gb6afec73d437f3-dbreto1alejod.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/message/message",
-        type:"POST",
-        data:data,
+        url:"http://localhost:8080/api/Client/all",
+        type:"GET",
         datatype:"JSON",
         success:function(respuesta){
-            $("#resultado").empty();
-            $("#id").val("");
-            $("#messagetext").val("");
-            cargarInfoMessage();
-            alert("Se Agrego Nuevo Mensaje Exitosamente")
+            respuesta.forEach(function(item){
+                var option = document.createElement('option');
+                option.value = item.idClient;
+                option.innerHTML = item.name;
+                idClient.appendChild(option)
+                })
         }
     });
+}
+
+function selectBarcos(){
+    $.ajax({
+        url:"http://localhost:8080/api/Boat/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            respuesta.forEach(function(item){
+                var option = document.createElement('option');
+                option.value = item.id;
+                option.innerHTML = item.name;
+                boat.appendChild(option)
+                })
+        }
+    });
+}
+
+function newMessage(){
+    if($("#idClient").val()){
+        if($("#boat").val()){
+            let objCliente = {idClient:$("#idClient").val()};
+            let objBarco = {id:$("#boat").val()};
+            let data={
+                messageText:$("#messageText").val(),
+                client:objCliente,
+                boat:objBarco,
+            };
+            let dataToSend=JSON.stringify(data);
+            $.ajax({
+                type:"POST",
+                contentType: "application/json; charset=utf-8",
+                data:dataToSend,
+                datatype:"JSON",
+                url:"http://localhost:8080/api/Message/save",
+                success:function(respuesta){
+                    $("#resultado").empty();
+                    $("#id").val("");
+                    $("#messageText").val("");
+                    $("#idClient").val("");
+                    $("#boat").val("");
+                    cargarInfoMessage();
+                    alert("Se Agrego Nuevo Mensaje Exitosamente")
+                }
+            });
+        }else {
+            alert("ojo, Debe seleccionar un barco")
+        }
+    }else {
+        alert("ojo, Debe seleccionar un cliente")
+    }
 }
 
 function passEditMensaje(){
@@ -335,4 +472,76 @@ function deleteMessage(idElemento){
             alert("Se Elimino Mensaje Exitosamente")
         }
     });
+}
+
+/* ------------------------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------------------------------------------- */
+
+function cargarInfoReservas(){
+    $.ajax({
+        type:"GET",
+        contentType: "application/json; charset=utf-8",
+        datatype:"JSON",
+        url:"http://localhost:8080/api/Reservation/all",
+        success:function(respuesta){
+            console.log(respuesta);
+            cargarRespuestaReservas(respuesta);
+        }
+
+    });
+}
+
+function cargarRespuestaReservas(items){
+    $("tbody").children().remove()
+    var tableBody = $('#tblReservas tbody');
+    var filaTabla = "";
+    for (var i=0; i<items.length; i++) {
+        filaTabla += "<tr>";
+        filaTabla += "<td>"+items[i].idReservation+"</td>";
+        filaTabla += "<td>"+items[i].startDate+"</td>";
+        filaTabla += "<td>"+items[i].devolutionDate+"</td>";
+        filaTabla += "<td>"+items[i].client.name+"</td>";
+        filaTabla += "<td>"+items[i].boat.name+"</td>";
+        filaTabla += "<td>"+items[i].status+"</td>";
+        //filaTabla +="<td> <button class='btn btn-primary btn-sm btnSelect' onclick='passEditMensaje()'>Editar</button><button style='margin-left: 10px' class='btn btn-danger btn-sm' onclick='deleteMessage("+items[i].id+")'>Eliminar</button> </td>";
+        filaTabla +="</tr>";
+    }
+    tableBody.append(filaTabla);
+}
+
+function newReserva(){
+    if($("#idClient").val()){
+        if($("#boat").val()){
+            let objCliente = {idClient:$("#idClient").val()};
+            let objBarco = {id:$("#boat").val()};
+            let data={
+                startDate:$("#startDate").val(),
+                devolutionDate:$("#devolutionDate").val(),
+                client:objCliente,
+                boat:objBarco,
+            };
+            let dataToSend=JSON.stringify(data);
+            $.ajax({
+                type:"POST",
+                contentType: "application/json; charset=utf-8",
+                data:dataToSend,
+                datatype:"JSON",
+                url:"http://localhost:8080/api/Reservation/save",
+                success:function(respuesta){
+                    $("#resultado").empty();
+                    $("#id").val("");
+                    $("#startDate").val("");
+                    $("#devolutionDate").val("");
+                    $("#idClient").val("");
+                    $("#boat").val("");
+                    cargarInfoMessage();
+                    alert("Se Agrego Nueva Reserva Exitosamente")
+                }
+            });
+        }else {
+            alert("ojo, Debe seleccionar un barco")
+        }
+    }else {
+        alert("ojo, Debe seleccionar un cliente")
+    }
 }
